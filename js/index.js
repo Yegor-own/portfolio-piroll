4d7b0da31c8d6c0795a60b017c2eb071e64657ca
Yegor-own <68266911+Yegor-own@users.noreply.github.com>
2021-05-23 18:02:58 +0500

    menu burger

---
+++ js/index.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  $('.navbar__burger').click(function(event) {
+    $('.navbar__burger,.navbar__menu').toggleClass('active');
+    $('body').toggleClass('lock')
+  });
+});
